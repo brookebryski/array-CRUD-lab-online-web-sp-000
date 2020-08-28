2d3296eaa48d343a4801c658fc3dd37ee82c4aae
chic-state-0895 <brookeabryski@gmail.com>
2020-08-28 19:56:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,9 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   ["blue", "yellow", "orange", "green","arrays!"]
-  green_color = array.pop
-  puts green_color.inspect
-  puts green_color
+.pop
 end
 
 def remove_element_from_start_of_array(array)
