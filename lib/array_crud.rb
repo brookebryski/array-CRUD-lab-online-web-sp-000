1571eb3bbcd070048f9c3e33309694bf7990bbb2
chic-state-0895 <brookeabryski@gmail.com>
2020-08-28 19:51:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,11 +9,11 @@ end
 def add_element_to_end_of_array(array, element)
  ["blue", "yellow", "orange", "green"]
  .push("arrays!")
-
 end
 
 def add_element_to_start_of_array(array, element)
-
+  ["blue", "yellow", "orange", "green"]
+  .unshift("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
