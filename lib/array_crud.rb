e81060aec356c61df4af0f02f4d20094b4cfe591
chic-state-0895 <brookeabryski@gmail.com>
2020-08-28 19:57:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,13 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  ["blue", "yellow", "orange", "green","arrays!"]
+  ["wow","blue", "yellow", "orange", "green","arrays!"]
 .pop
 end
 
 def remove_element_from_start_of_array(array)
-
+["wow","blue", "yellow", "orange", "green","arrays!"]
+.shift
 end
 
 def retrieve_element_from_index(array, index_number)
