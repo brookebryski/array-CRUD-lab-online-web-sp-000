615c38b6f6dcb3e576b94a04e67c3d3a852a3816
chic-state-0895 <brookeabryski@gmail.com>
2020-08-28 19:50:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+ ["blue", "yellow", "orange", "green"]
+ .push("red")
+ 
 end
 
 def add_element_to_start_of_array(array, element)
